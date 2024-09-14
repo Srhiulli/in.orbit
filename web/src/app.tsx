b8ebefa2dd70f1020317835001b9ff6ce99acff7
Srhiulli <srhiulli@gmail.com>
2024-09-14 13:05:57 -0300

    add ui components

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,7 @@
 import letsStart from "./assets/lets-start-ilustration.svg";
 import logo from "./assets/logo-in-orbit.svg";
 import { Plus } from "lucide-react";
+import { Button } from "./components/ui/button";
 export function App() {
 	return (
 		<div className="h-screen flex flex-col items-center justify-center gap-8">
@@ -10,13 +11,11 @@ export function App() {
 				Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
 				mesmo?
 			</p>
-			<button
-				type="button"
-				className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-			>
+			<Button>
+				{" "}
 				<Plus className=" size-4" />
 				Cadastrar meta
-			</button>
+			</Button>
 		</div>
 	);
 }
