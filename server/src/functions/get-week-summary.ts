ddb24e0918f2e457a7726da000a2fa831c05087b
Srhiulli <srhiulli@gmail.com>
2024-09-15 18:52:55 -0300

    add type summary response and use fetch with tanStack

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -1,74 +1,89 @@
 import { and, eq, gte, lte, sql } from "drizzle-orm";
-import { goals, goalsCompletions } from "../db/schema"
-import dayjs from "dayjs"
-import { db } from "../db"
+import { goals, goalsCompletions } from "../db/schema";
+import dayjs from "dayjs";
+import { db } from "../db";
 
 export async function getWeekSummary() {
-    const lastDayOfWeek = dayjs().endOf('week').toDate()
-    const firstDayOfWeek = dayjs().startOf('week').toDate()
+	const lastDayOfWeek = dayjs().endOf("week").toDate();
+	const firstDayOfWeek = dayjs().startOf("week").toDate();
 
-    const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
-      db
-        .select({
-          id: goals.id,
-          title: goals.title,
-          desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
-          createdAt: goals.createdAt,
-        })
-        .from(goals)
-        .where(lte(goals.createdAt, lastDayOfWeek))
-    )
+	const goalsCreatedUpToWeek = db.$with("goals_created_up_to_week").as(
+		db
+			.select({
+				id: goals.id,
+				title: goals.title,
+				desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+				createdAt: goals.createdAt,
+			})
+			.from(goals)
+			.where(lte(goals.createdAt, lastDayOfWeek)),
+	);
 
-      const goalsCompletedInWeek = db.$with('goals_completed_in_Week').as(
-        db
-          .select({
-            id: goalsCompletions.id,
-            title: goals.title,
-            completedAt: goalsCompletions.createdAt,
-            completedAtDate: sql`
+	const goalsCompletedInWeek = db.$with("goals_completed_in_Week").as(
+		db
+			.select({
+				id: goalsCompletions.id,
+				title: goals.title,
+				completedAt: goalsCompletions.createdAt,
+				completedAtDate: sql`
             DATE(${goalsCompletions.createdAt})
-            `.as('completedAtDate')
-          })
-          .from(goalsCompletions)
-          .innerJoin(goals, eq(goalsCompletions.goalId, goals.id))
-          .where(
-            and(
-            gte(goalsCompletions.createdAt, firstDayOfWeek),
-            lte(goalsCompletions.createdAt, lastDayOfWeek)
-          ))
-    );
-      
-    const goalsCompletedByWeekDay = db.$with('goals_completed_by_WeekDay').as(
-        db
-        .select({
-          completedAtDate: goalsCompletedInWeek.completedAtDate,
-          completions: sql `
+            `.as("completedAtDate"),
+			})
+			.from(goalsCompletions)
+			.innerJoin(goals, eq(goalsCompletions.goalId, goals.id))
+			.where(
+				and(
+					gte(goalsCompletions.createdAt, firstDayOfWeek),
+					lte(goalsCompletions.createdAt, lastDayOfWeek),
+				),
+			),
+	);
+
+	const goalsCompletedByWeekDay = db.$with("goals_completed_by_WeekDay").as(
+		db
+			.select({
+				completedAtDate: goalsCompletedInWeek.completedAtDate,
+				completions: sql`
           JSON_AGG(
           JSON_BUILD_OBJECT(
           'id', ${goalsCompletedInWeek.id},
           'title', ${goalsCompletedInWeek.title},
           'completedAt', ${goalsCompletedInWeek.completedAt}
           ))
-          `.as('completions')
-        })
-        .from(goalsCompletedInWeek)
-        .groupBy(goalsCompletedInWeek.completedAtDate)
-      )
+          `.as("completions"),
+			})
+			.from(goalsCompletedInWeek)
+			.groupBy(goalsCompletedInWeek.completedAtDate),
+	);
 
-    const result = await db
-      .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
-      .select({
-        completed: sql` (SELECT COUNT(*) FROM ${goalsCompletedInWeek}) `.mapWith(Number),
-        total: sql` (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek}) `.mapWith(Number),
-        goalsPerDay: sql`
+	type GoalsPerDay = Record<
+		string,
+		{
+			id: string;
+			title: string;
+			completedAt: string;
+		}[]
+	>;
+
+	const result = await db
+		.with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
+		.select({
+			completed: sql` (SELECT COUNT(*) FROM ${goalsCompletedInWeek}) `.mapWith(
+				Number,
+			),
+			total:
+				sql` (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek}) `.mapWith(
+					Number,
+				),
+			goalsPerDay: sql<GoalsPerDay>`
           JSON_OBJECT_AGG(
             ${goalsCompletedByWeekDay.completedAtDate},
             ${goalsCompletedByWeekDay.completions}
           )
-        `
-      })
-      .from(goalsCompletedByWeekDay)
-    return {
-        summary : result
-    }
+        `,
+		})
+		.from(goalsCompletedByWeekDay);
+	return {
+		summary: result[0],
+	};
 }
--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,13 +1,38 @@
 // import { EmptyGoals } from "./components/empty-goals";
+import { useEffect, useState } from "react";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
 import { Dialog } from "./components/ui/dialog";
+import { EmptyGoals } from "./components/empty-goals";
+import { UserSquare } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+
+type SummaryResponse = {
+	completed: number;
+	total: number;
+	goalsPerDay: Record<
+		string,
+		{
+			id: string;
+			title: string;
+			completedAt: string;
+		}[]
+	>;
+};
 
 export function App() {
+	const { data } = useQuery<SummaryResponse>({
+		queryFn: async () => {
+			const response = await fetch("http://localhost:3333/summary");
+			const data = await response.json();
+			return data.summary;
+		},
+		queryKey: ["summary"],
+	});
+
 	return (
 		<Dialog>
-			<Summary />
-			{/* <EmptyGoals /> */}
+			{data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 			<CreateGoal />
 		</Dialog>
 	);
